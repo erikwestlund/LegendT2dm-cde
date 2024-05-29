a3cbcd8086a61784b826be9dca6e8fbe2474c533
Westlund <e.westlund@northeastern.edu>
2024-05-29 17:13:41 -0400

    custom causal inference code

--- extra/CI/1 - run.R
+++ extra/CI/1 - run.R
@@ -5,13 +5,14 @@ options(java.parameters = c("-Xms200g", "-Xmx200g"))
 library(LegendT2dm)
 library(dbplyr)
 library(dplyr)
+library(keyring)
 
 # Configure and download database drivers. These will need to be unzipped.
 databaseDriversDir <- "/Users/e.westlund/Documents/DatabaseDrivers/"
 dir.create(databaseDriversDir, showWarnings = FALSE)
 Sys.setenv(DATABASECONNECTOR_JAR_FOLDER=databaseDriversDir)
 
-DatabaseConnector::downloadJdbcDrivers(dbms = "redshift")
+#DatabaseConnector::downloadJdbcDrivers(dbms = "redshift")
 
 # Configure temp folders
 andromedaTempDir <- "/Users/e.westlund/Documents/AndromedaTemp/"
@@ -20,36 +21,136 @@ options(andromedaTempFolder = andromedaTempDir)
 oracleTempSchema <- NULL
 
 # Results folder
-resultsDir <- "/Users/e.westlund/Documents/Legendt2dmResults"
+resultsDir <- "/Users/e.westlund/Documents/Legendt2dmCiResults"
 dir.create(resultsDir, showWarnings = FALSE)
 
 # Configure
 cdmDatabaseSchema <- "omop_cdm_53_pmtx_202203"
 serverHostname <-
-serverSuffix <- "ohdsilab"
+serverSuffix <- "ohdsi_lab"
 cohortDatabaseSchema <- "work_e_westlund185"
 databaseId <- "NEOHDSI"
-databaseName <- "Northeastern OMOP CDM"
-databaseDescription <- "Northeastern OHDSI Lab OMOP CDM"
-tablePrefix <- "legend_t2dm_ne_ohdsilab"
+databaseName <- "Northeastern PharMetrics Plus"
+databaseDescription <- "Northeastern PharMetrics Plus OMOP CDM"
+tablePrefix <- "legend_t2dm_ci"
 outputFolder <- resultsDir
 maxCores <- 2
 
 # Before: Unzip downloaded Redshift drivers and make sure pathToDriver directory is correct
-conn <- DatabaseConnector::createConnectionDetails(
+connectionDetails <- DatabaseConnector::createConnectionDetails(
   dbms = "redshift",
-  server = "ohdsi-lab-redshift-cluster-prod.clsyktjhufn7.us-east-1.redshift.amazonaws.com/ohdsi_lab",
+  server = paste0(keyring::key_get("redshiftServer"), "/", !!serverSuffix),
   port = 5439,
-  user = "e_westlund185",
-  password = "XXXXXX",
+  user = keyring::key_get("redshiftUser"),
+  password = keyring::key_get("redshiftPassword"),
   pathToDriver = paste0(databaseDriversDir, "redShiftV1.2.27.1051")
 )
 
-indicationId <- "class"
+indicationId <- "ci"
 filterOutcomeCohorts <- NULL
 filterExposureCohorts <- NULL
 oracleTempSchema <- NULL
-connectionDetails <- conn
+vocabularyDatabaseSchema <- cdmDatabaseSchema
+
+# We will pull the necessary sections of execute in ./R/Main.R to generate required data.
+
+# Note: we swap out the indicationId from "class" to "ci" to be customized for what we want to study
+# This requires us to create some extra settings files. Namely:
+# - settings/ciCohortsToCreate.csv
+createExposureCohorts(connectionDetails = connectionDetails,
+                      cdmDatabaseSchema = cdmDatabaseSchema,
+                      vocabularyDatabaseSchema = vocabularyDatabaseSchema,
+                      cohortDatabaseSchema = cohortDatabaseSchema,
+                      tablePrefix = tablePrefix,
+                      indicationId = indicationId,
+                      oracleTempSchema = oracleTempSchema,
+                      outputFolder = outputFolder,
+                      databaseId = databaseId,
+                      filterExposureCohorts = filterExposureCohorts,
+                      imputeExposureLengthWhenMissing = imputeExposureLengthWhenMissing)
+
+# Note that I have modified this function to accept an Outcomes parameter, allowing us
+# to pull fewer outcomes.
+createOutcomeCohorts(connectionDetails = connectionDetails,
+                     cdmDatabaseSchema = cdmDatabaseSchema,
+                     vocabularyDatabaseSchema = vocabularyDatabaseSchema,
+                     cohortDatabaseSchema = cohortDatabaseSchema,
+                     tablePrefix = tablePrefix,
+                     outcomesFile = "settings/ciOutcomesOfInterest.csv",
+                     oracleTempSchema = oracleTempSchema,
+                     outputFolder = outputFolder,
+                     databaseId = databaseId,
+                     filterOutcomeCohorts = filterOutcomeCohorts)
+
+fetchAllDataFromServer(connectionDetails = connectionDetails,
+                       cdmDatabaseSchema = cdmDatabaseSchema,
+                       oracleTempSchema = oracleTempSchema,
+                       cohortDatabaseSchema = cohortDatabaseSchema,
+                       tablePrefix = tablePrefix,
+                       indicationId = indicationId,
+                       outputFolder = outputFolder,
+                       studyEndDate = studyEndDate,
+                       useSample = FALSE)
+
+
+generateAllCohortMethodDataObjects(outputFolder = outputFolder,
+                                   indicationId = indicationId,
+                                   useSample = FALSE,
+                                   maxCores = maxCores)
+
+if (runCohortMethod) {
+  runCohortMethod(outputFolder = outputFolder,
+                  indicationId = indicationId,
+                  databaseId = databaseId,
+                  maxCores = maxCores,
+                  runSections = runSections)
+}
+
+# if (computeIncidence) {
+#     computeIncidence(outputFolder = outputFolder, indicationId = indicationId)
+# }
+#
+# if (fetchChronographData) {
+#     fetchChronographData(connectionDetails = connectionDetails,
+#                          cdmDatabaseSchema = cdmDatabaseSchema,
+#                          oracleTempSchema = oracleTempSchema,
+#                          cohortDatabaseSchema = cohortDatabaseSchema,
+#                          tablePrefix = tablePrefix,
+#                          indicationId = indicationId,
+#                          outputFolder = outputFolder)
+# }
+
+if (computeCovariateBalance) {
+  computeCovariateBalance(outputFolder = outputFolder,
+                          indicationId = indicationId,
+                          maxCores = maxCores)
+}
+
+if (exportToCsv) {
+  exportResults(indicationId = indicationId,
+                outputFolder = outputFolder,
+                databaseId = databaseId,
+                databaseName = databaseName,
+                databaseDescription = databaseDescription,
+                minCellCount = minCellCount,
+                runSections = runSections,
+                maxCores = maxCores,
+                exportSettings = exportSettings)
+}
+
+ParallelLogger::logInfo(sprintf("Finished execute() for LEGEND-T2DM %s-vs-%s studies",
+                                indicationId, indicationId))
+
+
+
+
+
+## END execute() ####
+
+
+######
+
+# OLD:
 
 # Required to run study sections -- from Main.R
 writePairedCounts <- function(outputFolder, indicationId) {
